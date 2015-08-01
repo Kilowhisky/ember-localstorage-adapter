875f5c4eda47d93a76467d40307d0d2c65cd5f01
Chris Rice <c.rice@blueskynetwork.com>
2015-08-01 07:38:45 -0700

    Restored old extract methods for the time being.

--- localstorage_adapter.js
+++ localstorage_adapter.js
@@ -7,7 +7,9 @@
 
     /**
      * Invokes the new serializer API.
-     * This should be removed in 2.0
+     * This should be removed in 2.0.
+     *
+     * Turn this to false to use the old extract methods
      */
     isNewSerializerAPI: true,
 
@@ -86,6 +88,63 @@
     },
 
     /**
+   * Extracts whatever was returned from the adapter.
+   *
+   * If the adapter returns relationships in an embedded way, such as follows:
+   *
+   * ```js
+   * {
+   *   "id": 1,
+   *   "title": "Rails Rambo",
+   *
+   *   "_embedded": {
+   *     "comment": [{
+   *       "id": 1,
+   *       "comment_title": "FIRST"
+   *     }, {
+   *       "id": 2,
+   *       "comment_title": "Rails is unagi"
+   *     }]
+   *   }
+   * }
+   *
+   * this method will create separated JSON for each resource and then push
+   * them individually to the Store.
+   *
+   * In the end, only the main resource will remain, containing the ids of its
+   * relationships. Given the relations are already in the Store, we will
+   * return a JSON with the main resource alone. The Store will sort out the
+   * associations by itself.
+   *
+   * @method extractSingle
+   * @private
+   * @param {DS.Store} store the returned store
+   * @param {DS.Model} type the type/model
+   * @param {Object} payload returned JSON
+   */
+  extractSingle: function(store, type, payload) {
+    if (payload && payload._embedded) {
+      for (var relation in payload._embedded) {
+        var relType = type.typeForRelationship(relation,store);
+        var typeName = relType.modelName,
+            embeddedPayload = payload._embedded[relation];
+
+        if (embeddedPayload) {
+          if (Ember.isArray(embeddedPayload)) {
+            store.pushMany(typeName, embeddedPayload);
+          } else {
+            store.push(typeName, embeddedPayload);
+          }
+        }
+      }
+
+      delete payload._embedded;
+    }
+
+    return this.normalize(type, payload);
+  },
+
+    /**
      * This is exactly the same as extractSingle, but used in an array.
      *
      * @method extractSingle
@@ -111,6 +170,21 @@
       }.bind(this));
 
       return response;
+    },
+
+    /**
+     * This is exactly the same as extractSingle, but used in an array.
+     *
+     * @method extractSingle
+     * @private
+     * @param {DS.Store} store the returned store
+     * @param {DS.Model} type the type/model
+     * @param {Array} payload returned JSONs
+     */
+    extractArray: function(store, type, payload) {
+      return payload.map(function(json) {
+        return this.extractSingle(store, type, json);
+      }, this);
     }
 });
 
